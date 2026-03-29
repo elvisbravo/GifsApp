e215b5201add2a1562ec4d2d6ece8686682b5c55
70167122 <elvisbravo262@gmail.com>
2026-03-29 13:29:47 -0500

    feat: useRef para el re-render en los gifs

--- src/gifs/hooks/useGifs.tsx
+++ src/gifs/hooks/useGifs.tsx
@@ -1,19 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { getGifsByQuery } from "../actions/get-gifs-by-query.action";
 import type { Gif } from "../interfaces/gif.interface";
 
-const gifsCache: Record<string, Gif[]> = {};
+//const gifsCache: Record<string, Gif[]> = {};
 
 export const useGifs = () => {
 
     const [gifs, setGifs] = useState<Gif[]>([]);
     const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
+    const gifsCache = useRef<Record<string, Gif[]>>({});
+
 
     const handleTermClicked = async (term: string) => {
 
-        if(gifsCache[term]) {
-            setGifs(gifsCache[term]);
+        if(gifsCache.current[term]) {
+            setGifs(gifsCache.current[term]);
             return;
         }
 
@@ -34,7 +36,7 @@ export const useGifs = () => {
 
         setGifs(gifs);
 
-        gifsCache[query] = gifs;
+        gifsCache.current[query] = gifs;
     }
     
     return {
