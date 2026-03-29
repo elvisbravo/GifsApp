d42743bf826e4725b3bb0596ff743ba62a152cfb
70167122 <elvisbravo262@gmail.com>
2026-03-29 12:05:04 -0500

    feat: agregar custom hook para la busqueda de gifs

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,9 @@ import { createRoot } from 'react-dom/client'
 import { GitsApp } from './GitsApp'
 
 import './index.css'
-import { MyCounterApp } from './counter/components/MyCounterApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/*<GitsApp />*/}
-    <MyCounterApp />
+    <GitsApp />
   </StrictMode>,
 )
